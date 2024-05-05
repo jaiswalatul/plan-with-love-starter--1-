d9601347d739f66d84b0d1b2ef7274c6a7fb76a5
atul jaiswal <jaiswalatul018@gmai.com>
2024-05-05 23:54:14 +0530

    added love

--- src/App.js
+++ src/App.js
@@ -4,30 +4,31 @@ import Tours from "./Components/Tours";
 import { useState } from "react";
 
 const App = () => {
-  const [tours, setTours] = useState(data);
+ 
+  const [tours,setTours]=useState(data);
 
-  // when not intereseted is clicked this removeTour function is called and based on id passed in the function
-  // the filter method is used to remove that data belonging to passed id
   function removeTour(id){
-    const newTours = tours.filter( tour => tour.id !== id);
-    setTours(newTours);
+    const newTour=tours.filter(tour=>tour.id!=id);
+    setTours(newTour)
   }
 
-  // when all tours are clicked not interested then reinitializing all data to tours array
-  if(tours.length === 0){
+  if(tours.length ==0){
     return(
       <div className="refresh">
-        <h2>Sorry 😔...... <br/>No More Tours Left</h2>
-        <button onClick={() => setTours(data)} className="btn-white">Refresh</button>
+        <h2>No Tours Left</h2>
+        <button className="btn-white" onClick={() => setTours(data)}>
+          Refresh
+        </button>
       </div>
     )
   }
+
   return (
     <div className="App">
-     {/* removeTour and tours data are passed as props in tours component */}
-      <Tours tours={tours} removeTour={removeTour}> </Tours>
+      
+      <Tours tours={tours} removeTour={removeTour}></Tours>
     </div>
-  );
+  )
 };
 
 export default App;
--- src/Components/Tours.js
+++ src/Components/Tours.js
@@ -1,20 +1,19 @@
 import Card from "./Card";
 
-function Tours({ tours, removeTour }) {
+
+function Tours({tours,removeTour}){
+  
   return (
     <div className="container">
-      {/* heading */}
       <div>
         <h2 className="title">Plan With Love</h2>
       </div>
-      {/* content */}
-      <div className="cards">
-        {/* here instead of writing different cards for different cities in tours array we use map function */}
-        {/* key={tour.id} => it is a good practice to pass key while using map function */}
+
+      <div className="cards"> 
         {
-            tours.map((tour) => {
-            return <Card key={tour.id} {...tour} removeTour={removeTour}></Card>;
-           })
+            tours.map((tour)=>{
+               return <Card {...tour} removeTour={removeTour}></Card>
+            })
         }
       </div>
     </div>
